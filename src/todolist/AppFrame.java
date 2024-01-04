bb26649b4ec5e6716206f2ea7919c36baa8c5184
abdullah <abdullahiqbal599@gmail.com>
2024-01-04 22:38:51 +0500

    Add Frame as AppFrame

--- src/todolist/AppFrame.java
+++ src/todolist/AppFrame.java
@@ -4,10 +4,36 @@
  */
 package todolist;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Abdullah
  */
-class AppFrame {
+
+/**
+ *
+ * @author Abdullah
+ */
+public class AppFrame extends JFrame {
     
+  
+   
+   private JButton AddTask;
+   private JButton Clear;
+   private JButton Save;
+    
+    AppFrame(){
+        this.setSize(400,700);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("                Do To DO List ");
+        
+        
+        
+        
+    }
+    
+       
 }
