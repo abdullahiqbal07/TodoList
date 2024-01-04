dc433160fae957d980e7e1eea4435ae7af62e9fc
abdullah <abdullahiqbal599@gmail.com>
2024-01-04 21:45:08 +0500

    initialize my project

---
+++ src/todolist/TODOLIST.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package todolist;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class TODOLIST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         AppFrame appFrame = new AppFrame();
+    }
+    
+}
